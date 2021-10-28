cf24a2cb776da9f8f28fc48f45afd15c5a036fc8
hubert <hanwenhui520@gmail.com>
2021-10-29 00:25:49 +0800

    feat: update template-toolbar

--- src/views/quota/templateView/index.vue
+++ src/views/quota/templateView/index.vue
@@ -4,7 +4,7 @@
       <TemplateTree :show-search="true" class="h-full w-full" @selectNode="selectNode" />
     </div>
     <div class="flex-grow h-full enter-y bg-gray-100">
-      <ToolBar @update="updateConfig" />
+      <ToolBar />
       <Views class="views-box bg-white border" @selectTemplate="selectTemplate" />
     </div>
   </div>
@@ -14,15 +14,15 @@
   import { TemplateTree } from '/@/components/TemplateTree';
   import Views from './components/Views.vue';
   import ToolBar from './components/ToolBar.vue';
-  import { createTemplateListContext } from './hooks';
+  import { createSelectTemplateListContext, createTemplateListContext } from './hooks';
   import { ref } from 'vue';
   import type { TemplateDOM } from '/#/template';
   import { useUniqueField } from '../quotaTable/components/helper';
-  import { parseInt } from 'lodash';
 
   const templateList = ref<TemplateDOM[]>([]);
-  const uniqIds = ref<string[]>([]);
+  const selectedTemplateList = ref<TemplateDOM[]>([]);
   createTemplateListContext(templateList);
+  createSelectTemplateListContext(selectedTemplateList);
   const { getUniqueField } = useUniqueField();
   function selectNode(node: TemplateDOM) {
     node.uniqId = getUniqueField();
@@ -31,53 +31,9 @@
       height: '400px',
     };
     templateList.value.push(node);
-    console.log(node);
   }
-  function selectTemplate(ids: string[]) {
-    uniqIds.value = ids;
-  }
-  function updateConfig({ name, param }: { name: string; param: any }) {
-    console.log({ name, param });
-
-    switch (name) {
-      case 'baseSize':
-        let { width, height } = param;
-        if (/\%/i.test(width)) {
-          width = width;
-        } else if (parseInt(width).toString() === width) {
-          width = `${width}px`;
-        } else {
-          width = '33.3%';
-        }
-        if (parseInt(height).toString() === height) {
-          height = `${height}px`;
-        } else {
-          height = '300px';
-        }
-        templateList.value.forEach((temp) => {
-          if (uniqIds.value.includes(temp.uniqId)) {
-            temp.pageConfig.width = width;
-            temp.pageConfig.height = height;
-          }
-        });
-        break;
-      case 'date':
-        templateList.value.forEach((temp) => {
-          if (temp.version < 3 && uniqIds.value.includes(temp.uniqId)) {
-            [temp.config.timeConfig.startDate, temp.config.timeConfig.endDate] = [...param];
-          }
-        });
-        break;
-      case 'showLastest':
-        templateList.value.forEach((temp) => {
-          if (temp.version < 3 && uniqIds.value.includes(temp.uniqId)) {
-            temp.config.showLastest = param;
-          }
-        });
-        break;
-      default:
-        break;
-    }
+  function selectTemplate(arr: TemplateDOM[]) {
+    selectedTemplateList.value = arr;
   }
 </script>
 
